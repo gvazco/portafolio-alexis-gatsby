24336c5ebaed55555e2b1d795e9b85cb02ad3ccd
Gustavo <gustavovazco@gmail.com>
2021-11-05 13:40:31 -0600

    CardColumns

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { CardColumns, Card } from "react-bootstrap"
+import { Card, Row, Col } from "react-bootstrap"
 import { Link } from "gatsby"
 import { map } from "lodash"
-import moment from "moment"
+// import moment from "moment"
 import "moment/locale/es"
 import "./PostList.scss"
 
@@ -11,24 +11,26 @@ export default function PostList(props) {
   console.log(posts)
 
   return (
-    <CardColumns className="post-list">
+    <Row xs={1} md={2} lg={3} className="post-list">
       {map(posts, post => (
-        <Link to={`/${post.slug}`}>
-          <Card className="post-list__item text-center">
-            <Card.Img
-              variant="top"
-              src={post.featuredImage.node.localFile.publicURL}
-            />
-            <Card.ImgOverlay className="item-meta">
-              <div>
-                <Card.Title>{post.title}</Card.Title>
+        <Col>
+          <Link to={`/${post.slug}`}>
+            <Card className="post-list__item text-center">
+              <Card.Img
+                variant="top"
+                src={post.featuredImage.node.localFile.publicURL}
+              />
+              <Card.ImgOverlay className="item-meta">
+                <div>
+                  <Card.Title>{post.title}</Card.Title>
 
-                <Card.Subtitle>{post.categories.nodes.name}</Card.Subtitle>
-              </div>
-            </Card.ImgOverlay>
-          </Card>
-        </Link>
+                  <Card.Subtitle>{post.categories.nodes.name}</Card.Subtitle>
+                </div>
+              </Card.ImgOverlay>
+            </Card>
+          </Link>
+        </Col>
       ))}
-    </CardColumns>
+    </Row>
   )
 }
